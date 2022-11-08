0d825f6354c4b8ad44834ea991d14f3623408f4e
baselreal <basriibrahim67067@gmail.com>
2022-11-08 22:07:21 +0700

    responsive fixing

--- src/component/about/index.jsx
+++ src/component/about/index.jsx
@@ -5,23 +5,21 @@ import Back from "./Back.png";
 
 const About = () => {
   return (
-    <body className="bG2">
-      <div className="containerContent">
-        <div className="bgColor"></div>
-        <div className="buttonBack">
+    <div className="containerContent">
+      <div className="bgColor"></div>
+      <div className="first wrapAll">
+        <div className="first buttonBack">
           <Link to="/">
             <img src={Back} alt="back" />
           </Link>
         </div>
-        <div className="wrapAll">
-          <div className="contentTitle">About Us</div>
-          <div className="content">
-            More than 10 years, Arkademy Travel Services has grow at a steady pace and become one of Indonesia's leading travel agent offering a complete travel solution for both leisure and business travels. The company has over 500 staff
-            within 24 branches across the country
-          </div>
+        <div className="first contentTitle">About Us</div>
+        <div className="first content">
+          More than 10 years, Arkademy Travel Services has grow at a steady pace and become one of Indonesia's leading travel agent offering a complete travel solution for both leisure and business travels. The company has over 500 staff
+          within 24 branches across the country
         </div>
       </div>
-    </body>
+    </div>
   );
 };
 
